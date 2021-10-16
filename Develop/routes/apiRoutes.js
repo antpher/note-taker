f7e3e8665b611d130a120b9e6f9a8192788543bb
Anthony Her <antpher@gmail.com>
2021-10-16 14:44:09 -0400

    updated post route

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const { notes } = require("../db/db.json");
 const fs = require("fs");
+const path = require("path");
 
 // Get all notes
 router.get("/notes", (req, res) => {
-    res.json(data);
+    res.json(notes);
 });
 
 // Get notes by id
@@ -14,10 +15,11 @@ router.get("/notes/:id", (req, res) => {
 
 // Post notes
 router.post("/notes", (req, res) => {
-    req.body.id = notes.length.toString();
-    const note = createNewNote(req.body, notes);
-    fs.writeFile("../db/db.json", JSON.stringify(note))
-    res.json(notes);
+    // req.body.id = notes.length.toString();
+    const newNote = req.body;
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'),JSON.stringify({ newNote }));
+    console.log(newNote);
+    res.json(newNote);
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes.js');
-const htmlRoutes = require('./routes/htmlRoutes.js');
-const fs = require("fs");
+const htmlRoutes = require('./routes/htmlRoutes.js');;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
